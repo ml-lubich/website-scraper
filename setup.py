5a2ea674170ed3726d821bb6ffad8b5b775ec67c
sax-n-keys-coding <mishalubich007@berkeley.edu>
2025-01-10 02:36:22 -0800

    chore: Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="website-scraper",
-    version="0.1.0",
+    version="0.1.1",
     author="Misha Lubich",
     author_email="michaelle.lubich@gmail.com",
     description="A robust, multiprocessing-enabled web scraper",
